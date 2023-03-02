1a4feb2797ec720c5f8aca5092a1717c5630cbcd
lucaslagant <lucaslagant@gmail.com>
2023-03-02 16:47:39 +0100

    incription

--- bdd/requete.sql
+++ bdd/requete.sql
@@ -1,14 +1,17 @@
 -- Procédure voire les produits
-DELIMITER $
-
+-- on change le ; par $
+DELIMITER $ 
+-- Création de la procedure
 CREATE PROCEDURE showProduct()
 BEGIN
     SELECT * FROM produit;
 END$
-
+-- On repasse le $ en ;
 DELIMITER ;
 
+-- vérification du status de la procédure 
 SHOW PROCEDURE STATUS LIKE 'showProduct';
+-- Appelle de la procédure 
 CALL showProduct();
 
 -- Procédure ajouté les produits
@@ -52,10 +55,16 @@ DELIMITER ;
 SHOW PROCEDURE STATUS LIKE 'updateProduct';
 CALL updateProduct();
 
+
 -- Transaction ajouté les produits
+
+-- Début de la Transaction
 START TRANSACTION;
+-- 1er requete pour voir la table avant les changements 
 SELECT * FROM produit;
+-- Requete pour les changements sur la table 
 INSERT INTO `produit` (id_produit, nom_prod, desc_prod, prix_ht_prod, Id_sous_rubrique) values
     ('4','produit4','desc4','10.00','2'),
     ('5','produit5','desc5','10.00','1');
+-- 2eme requete pour voir la table après les changements     
 SELECT * FROM produit;
